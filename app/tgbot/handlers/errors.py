4695abd2ec8c669d0728fe9bca45a59843f3910b
Yuriy Chebyshev <bomzheg@gmail.com>
2025-12-28 17:33:03 +0300

    fixed unknown intent

--- app/tgbot/handlers/errors.py
+++ app/tgbot/handlers/errors.py
@@ -3,8 +3,10 @@ import logging
 from functools import partial
 
 from aiogram import Bot, Dispatcher
+from aiogram.filters import ExceptionTypeFilter
 from aiogram.types.error_event import ErrorEvent
 from aiogram.utils.markdown import html_decoration as hd
+from aiogram_dialog.api.exceptions import UnknownIntent
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +27,16 @@ async def handle(error: ErrorEvent, log_chat_id: int, bot: Bot) -> None:
         f"{hd.quote(json.dumps(error.update.dict(exclude_none=True), default=str)[:3500])}\n",
     )
 
+async def clear_unknown_intent(error: ErrorEvent, bot: Bot):
+    assert error.update.callback_query
+    assert error.update.callback_query.message
+    await bot.edit_message_reply_markup(
+        chat_id=error.update.callback_query.message.chat.id,
+        message_id=error.update.callback_query.message.message_id,
+        reply_markup=None,
+    )
+
 
 def setup_errors(dp: Dispatcher, log_chat_id: int) -> None:
+    dp.errors.register(clear_unknown_intent, ExceptionTypeFilter(UnknownIntent))
     dp.errors.register(partial(handle, log_chat_id=log_chat_id))
